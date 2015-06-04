b8d8d00a7fa5b7a99f4d625fdf9ddc99ce98f5e8
trmini <trang@luunguyen.net>
2015-06-04 01:20:20 -0700

    Add SQL usage queries for Add-ins

---
+++ SQL Queries/Office Add-ins/Agave Activations via JSOM.sql
@@ -0,0 +1,11 @@
+﻿SELECT 
+	MONTH(Date) as Month
+	, Platform
+	, MainHost
+	, COUNT(1) as Cnt
+FROM V_AppActivated_OSI
+
+WHERE MONTH(Date) > 2
+
+GROUP BY MONTH(Date), Platform, MainHost
+ORDER BY Month, Platform, MainHost
---
+++ SQL Queries/Office Add-ins/WAC Activations.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+	Host
+	, ProviderName
+	, AppName
+	, COUNT(1) as Cnt
+
+FROM V_AppLoadTime_WAC
+
+WHERE Month = 5
+
+GROUP BY Host, ProviderName, AppName
+ORDER BY Host, ProviderName, Cnt DESC
